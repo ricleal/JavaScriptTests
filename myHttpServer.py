9ac91a193febf5beb585e42125abcd00107376e6
Ricardo M. Ferraz Leal <ricleal@gmail.com>
2015-02-12 15:21:13 -0500

    JQuery dialog from json with post submit

---
+++ myHttpServer.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python
+
+"""
+Save this file as server.py
+>>> python server.py 0.0.0.0 8001
+serving on 0.0.0.0:8001
+
+or simply
+
+>>> python server.py
+Serving on localhost:8000
+
+You can use this to test GET and POST methods.
+
+"""
+
+import SimpleHTTPServer
+import SocketServer
+import logging
+import cgi
+
+import sys
+
+
+if len(sys.argv) > 2:
+    PORT = int(sys.argv[2])
+    I = sys.argv[1]
+elif len(sys.argv) > 1:
+    PORT = int(sys.argv[1])
+    I = ""
+else:
+    PORT = 8000
+    I = ""
+
+
+class ServerHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+
+    def do_GET(self):
+        logging.warning("======= GET STARTED =======")
+        logging.warning(self.headers)
+        SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
+
+    def do_POST(self):
+        logging.warning("======= POST STARTED =======")
+        logging.warning(self.headers)
+        logging.warning("======= POST VALUES =======")
+        length = int(self.headers.getheader('content-length'))
+        data = cgi.parse_qs(self.rfile.read(length), keep_blank_values=1)
+        for i in data:
+            print i
+
+
+
+        logging.warning("\n")
+        SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
+
+Handler = ServerHandler
+
+httpd = SocketServer.TCPServer(("", PORT), Handler)
+
+print "@rochacbruno Python http server version 0.1 (for testing purposes only)"
+print "Serving at: http://%(interface)s:%(port)s" % dict(interface=I or "localhost", port=PORT)
+httpd.serve_forever()
