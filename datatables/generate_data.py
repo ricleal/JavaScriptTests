8699de228092a386c825ebd03c20f9894fec237b
Ricardo Ferraz Leal <ricleal@gmail.com>
2016-05-12 14:53:42 -0400

    Icat for hfir sans

---
+++ datatables/generate_data.py
@@ -0,0 +1,22 @@
+"""
+<th>Title</th>
+<th>S/Bg</th>
+<th>T/S</th>
+<th>Medium</th>
+<th>Dist</th>
+<th>Lambda</th>
+<th>Temp</th>
+"""
+
+dataset = []
+for a in ["Au","Plat","Alum"]:
+    for b in ["Sample","Background","Dual"]:
+        for c in ["Transmission","Scattering"]:
+            for d in ["D2O","H2O","BCA"]:
+                for e in ["6","12"]:
+                    for f in ["3","6", "12"]:
+                        for g in ["12","18","30"]:
+                            title = a+" "+b+" "+c+" in "+d+" dist="+e+"m Lambda="+f+" T="+g+"C."
+                            dataset.append([title,b,c,d,e,f,g])
+
+print dataset
