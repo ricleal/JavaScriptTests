ebb79ccee2051a3cf5b50730288ad8783222bcd0
Ricardo M. Ferraz Leal <ricleal@gmail.com>
2017-08-08 13:09:22 -0400

    CSV parsing with file check

--- CSV/ex1.js
+++ CSV/ex1.js
@@ -6,66 +6,66 @@ npm install --save-dev papaparse fs
 fs = require('fs');
 pp = require('papaparse');
 
-
 function beforeFirstChunk1D(chunk) {
-    // Split the text into rows
-    var rows = chunk.split(/\r\n|\r|\n/);
-   
-    var delimiterRegex = /([\s,]+)/g;
-    // Find the delimiter on 3rd row
-    var match = delimiterRegex.exec(rows[2]);
-    var delimiter = match[1];
-    var header = rows[0];
-   
-    if (header.startsWith("#")) {
-      header = header.replace(/#\s*/, '');
-      header = header.split(/[\s,]+/).join(delimiter);
-    } 
+  // Split the text into rows
+  var rows = chunk.split(/\r\n|\r|\n/);
 
-    rows[0] = header.toLowerCase();
-    // Remove the 2nd row if it's not data
-    if (rows[1].length <= 2) {
-      rows.splice(1, 1);
-    }
-    return rows.join("\r\n");
+  var delimiterRegex = /([\s,]+)/g;
+  // Find the delimiter on 3rd row
+  var match = delimiterRegex.exec(rows[2]);
+  var delimiter = match[1];
+  var header = rows[0];
+
+  if (header.startsWith("#")) {
+    header = header.replace(/#\s*/, '');
+    header = header.split(/[\s,]+/).join(delimiter);
   }
-// files endind in Iq.txt
-config1D =  {
-  header : true,
-  dynamicTyping : true, // parse string to int
-  delimiter : "",       // auto-detect
-  newline : "",         // auto-detect
-  quoteChar : '"',
-  skipEmptyLines: true,
-  beforeFirstChunk: beforeFirstChunk1D
+
+  rows[0] = header.toLowerCase();
+  // Remove the 2nd row if it's not data
+  if (rows[1].length <= 2) {
+    rows.splice(1, 1);
+  }
+  return rows.join("\r\n");
 }
+// files endind in Iq.txt
+config1D =
+    {
+      header : true,
+      dynamicTyping : true, // parse string to int
+      delimiter : "",       // auto-detect
+      newline : "",         // auto-detect
+      quoteChar : '"',
+      skipEmptyLines : true,
+      beforeFirstChunk : beforeFirstChunk1D
+    }
 
 function beforeFirstChunk2D(chunk) {
-    // Split the text into rows
-    var rows = chunk.split(/\r\n|\r|\n/);
-    var header = rows[0];
-    header = header.replace(/,/, '');
-    if (header.startsWith("Data columns")){
-      header = header.replace(/Data columns\s*/, '');
-      header = header.split(/[\s,-]+/).join("  ");
-    }
-    rows[0] = header.toLowerCase();
-    // Remove the 2nd row if it's not data
-    if (rows[1].split(/[\s,-]+/).length <= 2) {
-      rows.splice(1, 1);
-    }
-    return rows.join("\r\n");
+  // Split the text into rows
+  var rows = chunk.split(/\r\n|\r|\n/);
+  var header = rows[0];
+  header = header.replace(/,/, '');
+  if (header.startsWith("Data columns")) {
+    header = header.replace(/Data columns\s*/, '');
+    header = header.split(/[\s,-]+/).join("  ");
   }
+  rows[0] = header.toLowerCase();
+  // Remove the 2nd row if it's not data
+  if (rows[1].split(/[\s,-]+/).length <= 2) {
+    rows.splice(1, 1);
+  }
+  return rows.join("\r\n");
+}
 
 // files endind in Iqxy.dat
 config2D = {
   header : true,
   dynamicTyping : true, // parse string to int
   delimiter : "  ",
-  newline : "",         // auto-detect
+  newline : "", // auto-detect
   quoteChar : '"',
-  skipEmptyLines: true,
-  beforeFirstChunk: beforeFirstChunk2D
+  skipEmptyLines : true,
+  beforeFirstChunk : beforeFirstChunk2D
 }
 
 var data1D = fs.readFileSync(
@@ -77,21 +77,22 @@ var data2D = fs.readFileSync(
     '/home/rhf/git/SANSAnisotropic/Data/test-more-data-7-6-16/BioSANS_exp134_scan0012_0001_Iqxy.dat',
     'utf8');
 
-
-var results1D = pp.parse(
-  data1D,
-  config = config1D,
- );
+var results1D = pp.parse(data1D, config = config1D, );
 
 //  console.log(results1D);
 
- var results2D = pp.parse(
-  data2D,
-  config = config2D,
- );
+var results2D = pp.parse(data2D, config = config2D, );
 
- //console.log(results2D);
+// console.log(results2D);
 
- (/.*Iq.txt$/.exec("CG2_exp158_scan0039_0001_Iq.txt")) ?  console.log(results1D) : undefined;
+//
+// To check the file names:
+//
+var filename = "CG2_exp158_scan0039_0001_Iq.txt";
+// var filename = "BioSANS_exp134_scan0012_0001_Iqxy.dat";
 
- (/.*Iqxy.dat$/.exec("BioSANS_exp134_scan0012_0001_Iqxy.dat")) ?  console.log(results2D) : undefined;
+if (/.*Iq.txt$/.exec(filename))
+  console.log(results1D);
+else if (/.*Iqxy.dat$/.exec(filename))
+  console.log(results2D);
+else return undefined;
